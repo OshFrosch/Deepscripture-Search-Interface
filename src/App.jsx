1e471519b48f86d28f5a32a8dba0ba51cd26bc1d
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2022-02-05 17:45:53 +0100

    adds waiting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="background">
+      <div className="placeholder"></div>
+      <h1 className="title">deepscripture.com</h1>
+      <h2 className="sub-title">
+        A deep-learning search engine for the bible. Coming soon...
+      </h2>
+    </div>
+  );
 }
 
 export default App;
